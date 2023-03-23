02fa3f75c8f8c497835b0da80d274b04cb1701c7
Marcos Eskildecen <meskildecen@gmail.com>
2023-03-23 17:52:27 -0300

    ProjectPizza

---
+++ script.js
@@ -0,0 +1,6 @@
+pizzaJson.map((item, index)=>{
+    let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
+    //prencher as informações em pizzaitem
+
+    document.querySelector('.pizza-area').append();
+});
