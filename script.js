71c9351d15e5595e783e7527892362786395f746
Marcos Eskildecen <meskildecen@gmail.com>
2023-03-24 18:26:39 -0300

    select Type

--- script.js
+++ script.js
@@ -108,3 +108,9 @@ c('.pizzaInfo--qtmais').addEventListener('click', () => {
     c('.pizzaInfo--qt').innerHTML = modalQt;
 });
 
+cs('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');    
+    });
+});
